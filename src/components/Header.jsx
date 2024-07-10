7e8ca9bbde1e7fd8b58c85e2d6eafcee5c744284
Arvind Keshri <arvidce@gmail.com>
2024-07-10 21:49:16 +0530

    modularized context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Header.css";
 
 export const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark ">
       <div className="container-fluid">
         <a className="navbar-brand" href="#home">
           <h1>Colors</h1>
@@ -20,6 +23,12 @@ export const Header = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#cart">
+                <FontAwesomeIcon icon={faShoppingCart} className="fa" />
+                <span className="badge">{"9"}</span>
+              </a>
+            </li>
             <li className="nav-item">
               <a className="nav-link" href="#home">
                 Home
